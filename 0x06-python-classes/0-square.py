740717895cb76c8b82cfa2034b8a20e3b904b4e3
JohnIanOngayi <ongayijohnian@gmail.com>
2023-12-19 18:11:19 +0300

    Adding a file that constructs a class square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""A class representing a square"""
+
+
+class Square:
+    """
+    Constructs a square
+    """
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,3 @@
+# 0x06-python-classes
+
+An elementary introduction to classes and objects in Python
