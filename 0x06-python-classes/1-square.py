a3101025574cf208aecb0851800adc97977b8771
JohnIanOngayi <ongayijohnian@gmail.com>
2023-12-19 20:43:27 +0300

    Adding a function to define a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""A class defining a square"""
+
+
+class Square:
+    """A class defining a square"""
+    def __init__(self, __size):
+        """
+        Constructs a square
+        Args:
+        __size (int): The side of the square
+        """
+        self.__size = __size
