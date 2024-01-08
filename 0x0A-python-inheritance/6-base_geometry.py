2d779c281f4102b9ed56dafa1a1075033122a11f
JohnIanOngayi <ongayijohnian@gmail.com>
2024-01-08 16:16:32 +0300

    Adding a function that adds a method area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+
+Module creates class
+
+"""
+
+
+class BaseGeometry():
+    """Class BaseGeometry"""
+
+    def area(self):
+        """Raises an exception"""
+        raise Exception("area() is not implemented")
