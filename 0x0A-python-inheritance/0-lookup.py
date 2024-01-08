7e5ad212efcf9a52aa3efbedd6ef8270bcd3d0db
JohnIanOngayi <ongayijohnian@gmail.com>
2024-01-08 13:02:37 +0300

    Adding a function that returns the ist of available attributes and methods of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+
+This module returns the available attributes and methods of an object
+
+"""
+
+
+def lookup(obj):
+    """Returns attributes and methods of an object obj"""
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,3 @@
+# 0x0A-python-inheritance
+
+This repo folder contains programs that illustrate the concept of inheritance in Python OOP
