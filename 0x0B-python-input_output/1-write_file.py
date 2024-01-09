323c6c860e7143aaf750ef4e0c8ec53893bb1d6d
JohnIanOngayi <ongayijohnian@gmail.com>
2024-01-09 18:10:35 +0300

    Task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+
+Module writes text into a file
+
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file (UTF8)
+    Returns the number of characters written
+    """
+    with open(filename, "w", encoding='utf-8') as f:
+        return (f.write(text))
