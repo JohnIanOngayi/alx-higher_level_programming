13c2fab1b820a44fefc78e5b5c1173634b39485d
JohnIanOngayi <ongayijohnian@gmail.com>
2024-03-12 14:08:31 +0300

    Task 9

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+-- Script that creates table and adds multiple rows
+CREATE TABLE IF NOT EXISTS second_table(
+        id INT,
+        name VARCHAR(256),
+        score INT
+);
+
+INSERT into second_table
+VALUES (1, "John", 10), (2, "Alex", 3),
+       (3, "Bob", 14), (4, "George", 8);
