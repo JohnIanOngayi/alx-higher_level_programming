0e766ce7a17f1ed896413e76bd94cc4e628661c7
JohnIanOngayi <ongayijohnian@gmail.com>
2024-03-13 18:29:07 +0300

    Clean Up

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+
+MOdule creates a Pascal triangle
+
+"""
+
+
+def pascal_triangle(n):
+    """
+    Draws a Pascal's Triangle
+
+    n (int): the 'base' of the triangle
+    """
