5b4ec4d6cd2cfad07e260eccfba02ad3805873b2
JohnIanOngayi <ongayijohnian@gmail.com>
2024-03-13 18:23:08 +0300

    Task 8 corrections

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,10 @@
  -- Script that lists all values in a database
 
-SELECT id, name FROM cities
-WHERE state_id = (SELECT id FROM states WHERE name = 'California');
+SELECT id, name
+FROM cities
+WHERE state_id = (
+        SELECT id
+        FROM states
+        WHERE name = 'California'
+)
+ORDER BY cities.id ASC;
