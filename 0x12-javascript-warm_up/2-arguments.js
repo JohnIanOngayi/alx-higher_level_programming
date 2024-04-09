1e84479bf015c4c493161e33789f844978492bc5
JohnIanOngayi <ongayijohnian@gmail.com>
2024-04-09 08:37:31 +0300

    Task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/*
+Script prints message depending on no of cmd line args passed
+*/
+if (process.argv.length < 3) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
