ec52902649719c3e140ed75c51a8123f272fb913
JohnIanOngayi <ongayijohnian@gmail.com>
2024-04-11 11:47:09 +0300

    --amend

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,15 +4,28 @@
 Module queries a db to list all states
 """
 
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
 
-import MySQLdb
-import sys
+    conn = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3],
+        port=3306
+    )
 
-conn = MySQLdb.connect("localhost", sys.argv[1], sys.argv[2], sys.argv[3])
-c = conn.cursor()
-c.execute("SELECT * from states ORDER BY id")
-rows = c.fetchall()
-for eachRow in rows:
-    print(eachRow)
-c.close()
-conn.close()
+    c = conn.cursor()
+    c.execute(
+        """
+        SELECT * from states
+        ORDER BY states.id
+        """
+    )
+
+    rows = c.fetchall()
+    for eachRow in rows:
+        print(eachRow)
+    c.close()
+    conn.close()
