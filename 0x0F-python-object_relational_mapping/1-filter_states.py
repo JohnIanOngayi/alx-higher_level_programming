2504d7b4785bff80c6213fdf6a9557ee9e73581d
JohnIanOngayi <ongayijohnian@gmail.com>
2024-04-11 11:58:16 +0300

    Task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+Module queries a db to list all states
+Whose names start with N
+"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3],
+        port=3306
+    )
+
+    c = conn.cursor()
+    c.execute(
+        """
+        SELECT * from states
+        WHERE name LIKE BINARY 'N%'
+        ORDER BY states.id
+        """
+    )
+
+    rows = c.fetchall()
+    for eachRow in rows:
+        print(eachRow)
+    c.close()
+    conn.close()
