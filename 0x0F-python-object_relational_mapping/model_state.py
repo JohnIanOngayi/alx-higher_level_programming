5d97c8b20e0773166df1cc58e4563f51ff1bd3b9
JohnIanOngayi <ongayijohnian@gmail.com>
2024-04-13 21:23:07 +0300

    Task 6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+Module defines class states
+"""
+from sqlalchemy import String, Integer, Column
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    Class inherits from SQL class Base
+    it is used to define table for states
+    """
+    __tablename__ = "states"
+    id = Column(
+            "id",
+            Integer,
+            autoincrement=True,
+            primary_key=True,
+            unique=True,
+            nullable=False
+            )
+    name = Column("name", String(length=128), nullable=False)
