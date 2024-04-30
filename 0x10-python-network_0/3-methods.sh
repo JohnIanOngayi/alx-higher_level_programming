3e416ffa30f1de275495c18d33c4d2d6a0d7b091
JohnIanOngayi <ongayijohnian@gmail.com>
2024-04-30 14:09:44 +0300

    Adding spaces and trimming carriage character

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script prints allowed HTTP methods
-curl -sIX OPTIONS "$1" | grep -w Allow | awk '{print $2}'
+curl -sIX OPTIONS "$1" | grep -w Allow | awk '{print $2}' | sed 's/,/, /g' | tr -d '\r'
