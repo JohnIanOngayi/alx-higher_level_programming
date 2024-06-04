20ed7260e2a4ffddae07fa8caed860f5284960da
JohnIanOngayi <ongayijohnian@gmail.com>
2024-06-04 21:29:01 +0300

    Adding script that computes the number of tasks completed by user id

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+/*
+ * Script that computes the number of tasks completed by user id
+ */
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, { json: true }, (error, response, body) => {
+  if (error) console.log(error);
+  const results = {};
+  for (const task of body) {
+    if (task.completed) {
+      results[task.userId] = (results[task.userId] || 0) + 1;
+    }
+  }
+  console.log(results);
+});
